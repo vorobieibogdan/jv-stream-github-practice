65ed2d610563dda736237e63235d6dd33af83c1f
Bohdan Vorobiei <vbs@ukr.net>
2026-02-09 06:57:46 +0200

    The task is being done.3

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -1,9 +1,10 @@
 package practice;
 
-import java.util.Comparator;
+import java.util.Arrays;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 import model.Candidate;
 import model.Person;
 
@@ -11,44 +12,54 @@ public class StreamPractice {
 
     public int findMinEvenNumber(List<String> numbers) {
         return numbers.stream()
-                .flatMap(s -> List.of(s.split(",")).stream())
+                .flatMap(s -> Arrays.stream(s.split(",")))
                 .map(String::trim)
                 .mapToInt(Integer::parseInt)
                 .filter(n -> n % 2 == 0)
                 .min()
                 .orElseThrow(() ->
-                        new RuntimeException("Can't get min value from list: " + numbers));
+                        new NoSuchElementException(
+                                "Can't get min value from list: " + numbers));
     }
 
     public Double getOddNumsAverage(List<Integer> numbers) {
         return IntStream.range(0, numbers.size())
-                .map(i -> i % 2 == 1 ? numbers.get(i) - 1 : numbers.get(i))
+                .filter(i -> i % 2 == 1)
+                .map(numbers::get)
                 .filter(n -> n % 2 != 0)
                 .average()
                 .orElseThrow(NoSuchElementException::new);
     }
 
-    public List<Person> selectMenByAge(List<Person> peopleList, int fromAge, int toAge) {
+    public List<Person> selectMenByAge(List<Person> peopleList,
+                                       int fromAge,
+                                       int toAge) {
         return peopleList.stream()
-                .filter(p -> p.getSex() == Person.Sex.MAN)
-                .filter(p -> p.getAge() >= fromAge && p.getAge() <= toAge)
+                .filter(p -> p.getSex() == Person.Sex.MAN
+                        && p.getAge() >= fromAge
+                        && p.getAge() <= toAge)
                 .toList();
     }
 
-    public List<Person> getWorkablePeople(int fromAge, int femaleToAge,
-                                          int maleToAge, List<Person> peopleList) {
+    public List<Person> getWorkablePeople(int fromAge,
+                                          int femaleToAge,
+                                          int maleToAge,
+                                          List<Person> peopleList) {
         return peopleList.stream()
-                .filter(p -> p.getAge() >= fromAge)
-                .filter(p -> (p.getSex() == Person.Sex.MAN && p.getAge() <= maleToAge)
-                        || (p.getSex() == Person.Sex.WOMAN && p.getAge() <= femaleToAge))
+                .filter(p -> p.getAge() >= fromAge
+                        && ((p.getSex() == Person.Sex.MAN && p.getAge() <= maleToAge)
+                        || (p.getSex() == Person.Sex.WOMAN && p.getAge() <= femaleToAge)))
                 .toList();
     }
 
-    public List<String> getCatsNames(List<Person> peopleList, int femaleAge) {
+    public List<String> getCatsNames(List<Person> peopleList,
+                                     int femaleAge) {
         return peopleList.stream()
-                .filter(p -> p.getSex() == Person.Sex.WOMAN)
-                .filter(p -> p.getAge() >= femaleAge)
-                .flatMap(p -> p.getCats().stream())
+                .filter(p -> p.getSex() == Person.Sex.WOMAN
+                        && p.getAge() >= femaleAge)
+                .flatMap(p -> p.getCats() == null
+                        ? Stream.empty()
+                        : p.getCats().stream())
                 .map(cat -> cat.getName())
                 .toList();
     }
@@ -59,9 +70,8 @@ public class StreamPractice {
         return candidates.stream()
                 .filter(validator)
                 .map(Candidate::getName)
-                .sorted(Comparator.naturalOrder())
+                .sorted()
                 .toList();
     }
 }
-
 
